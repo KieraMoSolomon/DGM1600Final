6534c4aa6db03bcfa990c374d92365953df43b4b
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-04-11 11:23:18 -0600

    Figured out how to start the calculator

---
+++ DGM1600Final/Assets/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Calculator : MonoBehaviour {
+	double firstNumber;
+	double secondNumber;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	double Addition (){
+		double addit = firstNumber + secondNumber;
+		return;
+	}
+	double Subtraction (){
+		double subtract = firstNumber - secondNumber;
+		return;
+	}
+	double Mult (){
+		double mult = firstNumber * secondNumber;
+		return;
+	}
+	double Divide (){
+		double divide = firstNumber / secondNumber;
+		return;
+	}
+}
