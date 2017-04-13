76a1e77f0ddb543cef7d6f4a6a999f727c07d743
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-04-13 11:20:42 -0600

    Started over...

---
+++ DGM 1600 Final/Final Project/Assets/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Calculator : MonoBehaviour {
+	public double firstNumber;
+	public double secondNumber;
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+		double Addition (){
+			double addIt = firstNumber + secondNumber;
+			return addIt;
+		}
+		double Subtraction (){
+			double subtract = firstNumber - secondNumber;
+			return subtract;
+		}
+		double Mult (){
+			double mult = firstNumber * secondNumber;
+			return mult;
+		}
+		double Divide (){
+			double divide = firstNumber / secondNumber;
+			return divide;
+		}
+	void Operations(string type)
+	{
+		switch (type) {
+		case "+":
+			//adition
+			break;
+		case "-":
+			//subtraction
+			break;
+		case "*":
+			//mult
+			break;
+		case "/":
+			//devide
+			break;
+		}
+	}
+}
