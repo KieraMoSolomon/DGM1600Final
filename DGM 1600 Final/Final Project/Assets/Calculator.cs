ef87ae07ba8762f0963d28f1057801a5eae16cd6
Kiera Solomon <kieramorgensolomon@gmail.com>
2017-04-26 23:20:48 -0600

    planned out the random numbers for all levels

--- DGM 1600 Final/Final Project/Assets/Calculator.cs
+++ DGM 1600 Final/Final Project/Assets/Calculator.cs
@@ -7,20 +7,28 @@ public class Calculator : MonoBehaviour {
 	int[] levelOne = new int[10];
 	int[] levelTwo = new int[20];
 	int[] levelThree = new int[30];
-	double addIt;
-	double subtract;
-	double mult;
-	double divide;
+	double result;
 	bool divideButton = true;
 	bool multButton = true;
 	bool addButton = true;
 	bool subButton = true;
 	const int LEV_ONE_RAN_NUMS = 10;
+	const int LEV_TWO_RAN_NUMS = 20;
+	const int LEV_THR_RAN_NUMS = 30;
 
 
 		// Use this for initialization
 		void Start () {
-		
+		//setting the values for the arrays for each level
+		for (int i = 0; i < LEV_ONE_RAN_NUMS; i++) {
+			levelOne [i] = Random.Range (1, 20);
+		}
+		for (int i = 0; i < LEV_TWO_RAN_NUMS; i++) {
+			levelTwo [i] = Random.Range (1, 50);
+		}
+		for (int i = 0; i < LEV_THR_RAN_NUMS; i++) {
+			levelThree [i] = Random.Range (1, 100);
+		}
 
 		}
 
